ca3aba44fa3ebc90a17464e3c730cb71b2e04489
Knightein <oliver.mollitt123@gmail.com>
2022-09-16 16:27:34 +0100

    feat: added functionality, still need to remove effect on removal

---
+++ src/main/java/me/knightein/ninja100days/gameplay/NinjaHelmet.java
@@ -0,0 +1,25 @@
+package me.knightein.ninja100days.gameplay;
+
+import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.potion.PotionEffectType;
+
+public class NinjaHelmet implements Listener {
+    @EventHandler
+    public void onEquip(PlayerArmorChangeEvent event) {
+        Player player = event.getPlayer();
+        if (player.getInventory().getHelmet() != null) {
+            String name = PlainTextComponentSerializer.plainText().serialize(event.getPlayer().getInventory().getHelmet().displayName());
+            name = name.substring(1, name.length() - 1);
+            if (event.getSlotType() == PlayerArmorChangeEvent.SlotType.HEAD) {
+                if (name.equals("Ninja Helmet")) {
+                    player.sendMessage("Success");
+                    player.addPotionEffect(PotionEffectType.NIGHT_VISION.createEffect(1000000, 2));
+                }
+            }
+        }
+    }
+}
