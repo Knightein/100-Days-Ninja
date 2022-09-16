80ce317cfc5d3630d0a620e5d6cfff2a1d7d6950
Knightein <oliver.mollitt123@gmail.com>
2022-09-16 16:27:54 +0100

    feat: added NinjaHelmet to start-up

--- src/main/java/me/knightein/ninja100days/NinjaMod.java
+++ src/main/java/me/knightein/ninja100days/NinjaMod.java
@@ -1,6 +1,7 @@
 package me.knightein.ninja100days;
 
 import me.knightein.ninja100days.command.CreateNinjaArmour;
+import me.knightein.ninja100days.gameplay.NinjaHelmet;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NinjaMod extends JavaPlugin {
@@ -9,6 +10,7 @@ public final class NinjaMod extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getCommand("createninja").setExecutor(new CreateNinjaArmour());
+        getServer().getPluginManager().registerEvents(new NinjaHelmet(), this);
     }
 
     @Override
