de5e8be0911c2a48576f7564cc424543fc82e231
Knightein <oliver.mollitt123@gmail.com>
2022-09-28 20:17:09 +0100

    feat: added the crafting recipes

--- src/main/java/me/knightein/ninja100days/NinjaMod.java
+++ src/main/java/me/knightein/ninja100days/NinjaMod.java
@@ -4,6 +4,8 @@ import me.knightein.ninja100days.command.CreateNinja;
 import me.knightein.ninja100days.gameplay.NinjaChestplate;
 import me.knightein.ninja100days.gameplay.NinjaHelmet;
 import me.knightein.ninja100days.gameplay.SmokeBomb;
+import me.knightein.ninja100days.recipies.IronShuriken;
+import me.knightein.ninja100days.recipies.NinjaStars;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NinjaMod extends JavaPlugin {
@@ -15,6 +17,8 @@ public final class NinjaMod extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new NinjaHelmet(), this);
         getServer().getPluginManager().registerEvents(new NinjaChestplate(), this);
         getServer().getPluginManager().registerEvents(new SmokeBomb(), this);
+        getServer().addRecipe(new NinjaStars().woodNinjaStarRecipe());
+        getServer().addRecipe(new IronShuriken().ironNinjaStarRecipe());
     }
 
     @Override
