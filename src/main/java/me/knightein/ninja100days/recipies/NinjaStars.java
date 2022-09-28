c7fa0bde70216a61a98026640e2b39a072a98a11
Knightein <oliver.mollitt123@gmail.com>
2022-09-28 20:16:37 +0100

    fix: changed to NinjaStars and fully working

---
+++ src/main/java/me/knightein/ninja100days/recipies/NinjaStars.java
@@ -0,0 +1,27 @@
+package me.knightein.ninja100days.recipies;
+
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class NinjaStars {
+    public ShapedRecipe woodNinjaStarRecipe() {
+        ItemStack ninjaStar = new ItemStack(Material.SNOWBALL, 8);
+        ItemMeta ninjaStarMeta = ninjaStar.getItemMeta();
+
+        ninjaStarMeta.displayName(MiniMessage.miniMessage().deserialize(" <white>Wooden Ninja Star"));
+        ninjaStar.setItemMeta(ninjaStarMeta);
+
+        // Recipe
+        // ninja:star
+        ShapedRecipe ninjaStarRecipe = new ShapedRecipe(new NamespacedKey("ninja", "star"), ninjaStar);
+        ninjaStarRecipe.shape("*+*", "+++", "*+*");
+        ninjaStarRecipe.setIngredient('*', Material.AIR);
+        ninjaStarRecipe.setIngredient('+', Material.STICK);
+
+        return ninjaStarRecipe;
+    }
+}
