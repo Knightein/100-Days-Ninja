6d99ec00455839ae7423b18e0688ca7d85c48a0b
Knightein <oliver.mollitt123@gmail.com>
2022-09-30 00:10:30 +0100

    feat: added lifesteal

---
+++ src/main/java/me/knightein/ninja100days/gameplay/NinjaSword.java
@@ -0,0 +1,32 @@
+package me.knightein.ninja100days.gameplay;
+
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class NinjaSword implements Listener {
+    @EventHandler
+    public void lifeSteal(EntityDamageByEntityEvent event) {
+        // Basically says if it is a Player, do this.
+        if (event.getDamager().getType().equals(EntityType.PLAYER)) {
+            Player player = (Player) event.getDamager();
+            String name = PlainTextComponentSerializer.plainText().serialize(player.getInventory().getItemInMainHand().displayName());
+            name = name.substring(1, name.length() - 1);
+            if (name.equals("Grandmaster Sword") || name.equals("Ninja Sword")) {
+                double healingDamage = (event.getDamage() / 4);
+                player.setHealth(player.getHealth() + healingDamage);
+            }
+        }
+    }
+
+    public void bloodRush() {
+
+    }
+
+    public void cometDash() {
+
+    }
+}
