626d8ccf74a76cedf7edeac9795cfcaf960b74e6
Knightein <oliver.mollitt123@gmail.com>
2022-09-30 00:10:48 +0100

    feat: added ninja leggings functionality

--- src/main/java/me/knightein/ninja100days/gameplay/NinjaLeggings.java
+++ src/main/java/me/knightein/ninja100days/gameplay/NinjaLeggings.java
@@ -5,7 +5,9 @@ import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class NinjaLeggings implements Listener {
     private static boolean armourEquipped = false;
@@ -13,22 +15,28 @@ public class NinjaLeggings implements Listener {
     @EventHandler
     public void onEquip(PlayerArmorChangeEvent event) {
         Player player = event.getPlayer();
-        if (player.getInventory().getChestplate() != null) {
-            String name = PlainTextComponentSerializer.plainText().serialize(event.getPlayer().getInventory().getChestplate().displayName());
+        if (player.getInventory().getLeggings() != null) {
+            String name = PlainTextComponentSerializer.plainText().serialize(event.getPlayer().getInventory().getLeggings().displayName());
             name = name.substring(1, name.length() - 1);
-            if (event.getSlotType() == PlayerArmorChangeEvent.SlotType.CHEST) {
-                if (name.equals("Ninja Chestplate")) {
+            if (event.getSlotType() == PlayerArmorChangeEvent.SlotType.LEGS) {
+                if (name.equals("Ninja Leggings")) {
                     armourEquipped = true;
                 } else {
                     armourEquipped = false;
                 }
             }
+        } else {
+            armourEquipped = false;
         }
     }
 
     @EventHandler
-    public void wallClimb(PlayerMoveEvent event) {
+    public void goInvis(PlayerToggleSneakEvent event) {
         Player player = event.getPlayer();
-
+        if (!player.isSneaking() && armourEquipped) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 100000000, 1));
+        } else {
+            player.removePotionEffect(PotionEffectType.INVISIBILITY);
+        }
     }
 }
